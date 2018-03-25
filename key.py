3025e316e38db04624ed232cc01accacfc6ddd12
Vishal Kumar <vishukr785@gmail.com>
2018-03-25 17:23:33 +0530

    Delete key.py

--- key.py
+++
@@ -1,6 +0,0 @@
-# consumer token data
-def oauth():
-    return {"consumer_key": "hoW******************2Rf",
-            "consumer_secret": "gCSl*********************************FEnmt",
-            "token_key": "810***************************************QWwBQR",
-            "token_secret": "51c7*********************************PgugACi"}
