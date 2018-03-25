21e900771cae7716759c757abefe8d6465361a79
Vishal Kumar <vishukr785@gmail.com>
2018-03-25 17:24:52 +0530

    Update key.py

--- twitteruserstatus/key.py
+++ twitteruserstatus/key.py
@@ -1,6 +1,6 @@
 # consumer token data
 def oauth():
-    return {"consumer_key": "hoW******************2Rf",
+    return {"consumer_key": "hoW*****************2Rf",
             "consumer_secret": "gCSl*********************************FEnmt",
             "token_key": "810***************************************QWwBQR",
             "token_secret": "51c7*********************************PgugACi"}
