42d92d15e3f1067034dc4f57715e238ce2970d2b
VishalKr007 <vishukr785@gmail.com>
2018-03-25 16:31:02 +0530

    Initial commit

---
+++ twspider/key.py
@@ -0,0 +1,6 @@
+# consumer token data
+def oauth():
+    return {"consumer_key": "hoWNu6LXSRVkNMpcypzVhr2Rf",
+            "consumer_secret": "gCSlErqZgWY2ah0Z3CF7gQLPDnjy1dn2ZVJN3EAn0njMiFEnmt",
+            "token_key": "810148484829609984-v2LoYkoIOvHgHsiFcY9hGF4EbQWwBQR",
+            "token_secret": "51c7KbrG6F6I3XiSLslAiA5Otn2OQeJGx0MQ6oPgugACi"}
