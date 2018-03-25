754f02f72ce44fed97d46979b56950e22f6a8518
Vishal Kumar <vishukr785@gmail.com>
2018-03-25 14:50:03 +0530

    Update key.py

--- key.py
+++ key.py
@@ -1,6 +1,6 @@
 # consumer token data
 def oauth():
-    return {"consumer_key": "hoWNu6LXSRVkNMpcypzVhr2Rf",
-            "consumer_secret": "gCSlErqZgWY2ah0Z3CF7gQLPDnjy1dn2ZVJN3EAn0njMiFEnmt",
-            "token_key": "810148484829609984-v2LoYkoIOvHgHsiFcY9hGF4EbQWwBQR",
-            "token_secret": "51c7KbrG6F6I3XiSLslAiA5Otn2OQeJGx0MQ6oPgugACi"}
+    return {"consumer_key": "hoW******************2Rf",
+            "consumer_secret": "gCSl*********************************FEnmt",
+            "token_key": "810***************************************QWwBQR",
+            "token_secret": "51c7*********************************PgugACi"}
