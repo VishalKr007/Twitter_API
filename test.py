32e2f3ab252d13f5f126b84dbdf8e2d571e3bb72
VishalKr007 <vishukr785@gmail.com>
2018-03-25 14:31:05 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import urllib.request, urllib.parse, urllib.error
+from twurl import augment
+import ssl
+import pprint
+
+print('...calling twitter...')
+print('')
+url = augment('https://api.twitter.com/1.1/statuses/user_timeline.json',
+               {'screen_name': 'vishal_kr7', 'count': '2'} )
+print(url)
+
+# ignore certificate error
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+connection = urllib.request.urlopen(url, context=ctx)
+data = connection.read().decode()
+pprint.pprint(data)
+
+print('')
+print('*****************************************')
+headers = dict(connection.getheaders())
+pprint.pprint(headers)
